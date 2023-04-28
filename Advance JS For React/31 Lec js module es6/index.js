f3f3686ee5516d8419326e529d9f6d98840075e0
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-04-28 17:26:20 +0530

    javascript

---
+++ Advance JS For React/31 Lec js module es6/index.js
@@ -0,0 +1,15 @@
+// const t = require ("script.js");
+// import {sum,diff} from './script.js';
+// console.log(sum(25,25), diff(25,20));
+
+
+
+// const st = require ('script.js');
+// console.log(st);
+
+
+
+// const li = require ("script.js");
+// console.log(li);
+// console.log(li.sum(12,12), li.mul(2,2,2));
+
---
+++ Advance JS For React/31 Lec js module es6/script.js
@@ -0,0 +1,28 @@
+// const sum = (a,b) => {
+//     return a+b;
+// };
+
+// const diff = (a,b) => {
+//     return a-b;
+// };
+// export {sum,diff};
+
+
+
+
+
+// const sum = (a,b) => {
+//     return a+b;
+// };
+// exports.sum;
+
+
+
+
+exports.sum = (a,b) => {
+    return a+b;
+};
+
+exports.mul = (x,y,z) => {
+    return x*y*z;
+};
