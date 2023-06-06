117428fd0a75b26b4d6baf8f3fdedc2dc53bb0da
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-06-06 18:33:37 +0530

    React

---
+++ my-app7/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NewPost from "./components/NewPost";
+
+function App (){
+  return(
+    <div><NewPost /></div>
+  )
+}
+export default App;
---
+++ my-app7/src/components/NewPost.jsx
@@ -0,0 +1,59 @@
+// import React from 'react';
+// import classcss from './NewPost.module.css';
+
+// function NewPost() {
+//     function ChangeBodyHandler(event) {
+//         console.log(event.target.value);
+//     }
+//     return (
+//         <form action="" className={classcss.form}>
+//             <p>
+//                 <label htmlFor="body">Test</label>
+//                 <textarea name="text" id="body" required rows={3} onChange={ChangeBodyHandler}></textarea>
+//             </p>
+//             <p>
+//                 <label htmlFor="name">Your name</label>
+//                 <input type="text" id='name' required onChange={ChangeBodyHandler} />
+//             </p>
+//         </form>
+//     )
+// }
+
+// export default NewPost;
+
+
+
+
+
+// * Hooks :-
+
+// (1) usestate :-
+
+import { useState } from "react";
+import classcss from './NewPost.module.css';
+
+function NewPost() {
+    const [enteredBody, setEnteredBody] = useState('');
+    function ChangeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    return (
+        <form action="" className={classcss.form}>
+            <p>
+                <label htmlFor="body">Test</label>
+                <textarea name="text" id="body" required rows={3} onChange={ChangeBodyHandler}></textarea>
+            </p>
+            <p>{enteredBody}</p>
+            <p>
+                <label htmlFor="name">Your name</label>
+                <input type="text" id='name' required onChange={ChangeBodyHandler} />
+            </p>
+        </form>
+    )
+
+
+}
+export default NewPost;
+
+
+
