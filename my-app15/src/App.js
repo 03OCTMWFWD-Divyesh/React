065225b2a4868b23f7aa3c35e4b473ff6ef60a27
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-07-04 15:00:16 +0530

    React

--- my-app15/src/App.js
+++ my-app15/src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Home from "./Component/Home";
 import BookList from "./Component/BookList";
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books" element={<BookList />} />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
   )
-}
+};
+
 export default App;
