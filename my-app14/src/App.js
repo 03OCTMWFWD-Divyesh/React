ce292a874504db7e6316eeed8afe347aad498c32
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-07-08 14:41:47 +0530

    React

--- my-app14/src/App.js
+++ my-app14/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { usecallback, useMemo, useState } from "react";
 import List from './component/List';
 
@@ -6,28 +6,28 @@ function App() {
     const [number, setNumber] = useState(1);
     const [dark, setDark] = useState(false);
 
-    // const getItem = () => {
+    // const getItems = () => {
     //     return [number, number + 1, number + 2];
     // }
 
 
-    // const getItem = usecallback(() => {
+    // const getItems = usecallback(() => {
     //     return [number, number + 1, number + 2];
     // }, [number]);
 
 
-    // const getItem = useMemo (() => {
+    // const getItems = useMemo (() => {
     //     return [number, number+1, number+2];
     // }, [number]);
 
 
-    const getItem = usecallback ((increment) => {
-        return [number+increment, number+1+increment, number+2+increment];
+    const getItems = useCallback((increment) => {
+        return [number + increment, number + 1 + increment, number + 2 + increment];
     }, [number]);
 
 
     const theme = {
-        backgroundcolor: dark ? '#333' : '#fff',
+        backgroundColor: dark ? '#333' : '#fff',
         color: dark ? '#fff' : '#333'
     };
 
@@ -37,7 +37,7 @@ function App() {
             <div style={theme}>
                 <input type='number' value={number} onChange={e => setNumber(parseInt(e.target.value))} />
                 <button onClick={() => setDark(prevDark => !prevDark)}>Theme Change</button>
-                <List getItems={getItem} />
+                <List getItems={getItems} />
             </div>
         </div>
     );
