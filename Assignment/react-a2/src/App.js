331fa199fd3e4871d0e7fdce4c3d22c2240ba5bb
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-07-21 18:23:19 +0530

    React

---
+++ Assignment/react-a2/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Section from './Components/Section';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
